1ac5746b493cba98ad00a5167e4bfb39b45d77cb
userwww <you@example.com>
2021-05-09 23:07:41 +0300

    v1

---
+++ src/ua/matyukhin/hw4/Task4.java
@@ -0,0 +1,24 @@
+package ua.matyukhin.hw4;
+
+import java.util.Arrays;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int) (Math.random() * (1 - 10) + 10);
+        }
+        System.out.println("Исходный массив: " + Arrays.toString(numbers));
+        System.out.println("Замена четных:  " + Arrays.toString(isEven(numbers)));
+    }
+
+    public static int[] isEven(int[] numbers) {
+        int[] workArray = Arrays.copyOf(numbers, numbers.length);
+        for (int i = 0; i < workArray.length; i++) {
+            if (workArray[i] % 2 == 0) {
+                workArray[i] = 0;
+            }
+        }
+        return workArray;
+    }
+}
