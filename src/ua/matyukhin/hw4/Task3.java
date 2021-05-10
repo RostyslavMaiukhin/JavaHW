fd96041419dbb110e855f73f7c097604afcd3929
userwww <you@example.com>
2021-05-10 10:37:17 +0300

    v3

--- src/ua/matyukhin/hw4/Task3.java
+++ src/ua/matyukhin/hw4/Task3.java
@@ -2,28 +2,28 @@ package ua.matyukhin.hw4;
 
 import java.util.Arrays;
 
-public class Task2 {
+public class Task3 {
     public static void main(String[] args) {
-        int[] numbers = new int[10];
+        int[] numbers = new int[1000];
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = (int) (Math.random() * (1 - 10) + 10);
+            numbers[i] = (int) (Math.random() * (1 - 100) + 100);
         }
         System.out.println("Исходный массив " + Arrays.toString(numbers));
 
-        System.out.println("Протсых чисел: " + primeNumbers(numbers));
+        System.out.println("Составных чисел: " + compositedNumber(numbers));
     }
 
-    public static int primeNumbers(int[] numbers) {
+    public static int compositedNumber(int[] numbers) {
         int counter = 0;
-        int counterSimple=0;
+        int counterSimple = 0;
         int[] workArray = Arrays.copyOf(numbers, numbers.length);
         for (int k : workArray) {
-            for (int j = 1; j < k; j++) {
+            for (int j = 2; j < k; j++) {
                 if (k % j == 0) {
                     counter++;
                 }
             }
-            if (counter == 1) {
+            if (counter >= 1) {
                 System.out.println(k);
                 counterSimple++;
             }
