0a50ce1dc294bd486bb963b750eb1d9623fb31da
userwww <you@example.com>
2021-05-12 14:18:11 +0300

    v1

---
+++ src/ua/matyukhin/hw5/Task1.java
@@ -0,0 +1,25 @@
+package ua.matyukhin.hw5;
+
+import java.util.Arrays;
+
+
+public class Task1 {
+    public static void main(String[] args) {
+        int[][] numbers = new int[5][5];
+        fillArray(numbers);
+        System.out.println(Arrays.deepToString(numbers).replace("], ", "]\n"));
+    }
+
+    public static void fillArray(int[][] numbers) {
+        int counter = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                numbers[i][j] = counter;
+                if (i!=0){
+                    numbers[i][j] = -counter;
+                }
+                counter++;
+            }
+        }
+    }
+}
