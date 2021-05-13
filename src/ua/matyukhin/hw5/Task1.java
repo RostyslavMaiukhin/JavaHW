86f49f064d73a90e22d82cfe48be1ff3e555607e
userwww <you@example.com>
2021-05-13 22:03:38 +0300

    v1.00000000002

--- src/ua/matyukhin/hw5/Task1.java
+++ src/ua/matyukhin/hw5/Task1.java
@@ -5,11 +5,10 @@ import java.util.Arrays;
 public class Task1 {
     public static void main(String[] args) {
         int[][] numbers = new int[5][5];
-        fillArray(numbers);
-        System.out.println(Arrays.deepToString(numbers).replace("], ", "]\n"));
+        System.out.println(Arrays.deepToString(fillArrayDiversity(numbers)).replace("], ", "]\n"));
     }
 
-    private static void fillArray(int[][] numbers) {
+    private static int[][] fillArrayDiversity(int[][] numbers) {
         int counter = 0;
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < numbers[i].length; j++) {
@@ -17,5 +16,6 @@ public class Task1 {
                 counter++;
             }
         }
+        return numbers;
     }
 }
