a06fe105870f43c9be5815028eb1e6aad62807ef
userwww <you@example.com>
2021-05-17 12:13:37 +0300

    v1

---
+++ src/ua/matyukhin/hw6/Task2.java
@@ -0,0 +1,36 @@
+package ua.matyukhin.hw6;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        int[][] triangle = new int[5][];
+
+        triangle[0] = new int[1];
+        triangle[1] = new int[2];
+        triangle[2] = new int[3];
+        triangle[3] = new int[4];
+        triangle[4] = new int[5];
+
+        for (int i = 0; i < triangle.length; i++) {
+//            System.out.println(triangle.length + "!");
+//            System.out.println("i = " + i);
+            for (int j = 0; j < triangle[i].length; j++) {
+//                System.out.println(triangle[i].length);
+                triangle[i][j] = 0;
+            }
+
+        }
+/* Изначально компилятор сам показал строку где он уходит в OOB (31),
+ я сделал BP на цикле (29) и пошел по StepInto,увидел что на второй итерации он спотыкается на выводе
+ изза неверных условий второго For, я заменил i на j,все было кончено.
+ */
+
+
+        for (int i = 0; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                System.out.print(triangle[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
