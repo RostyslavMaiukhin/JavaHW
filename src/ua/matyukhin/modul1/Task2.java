814dd7b9cbe072321815a2981f50d4f04c56d7a0
userwww <you@example.com>
2021-05-22 18:44:28 +0300

    work prototype (-10 lines)

--- src/ua/matyukhin/modul1/Task2.java
+++ src/ua/matyukhin/modul1/Task2.java
@@ -1,6 +1,7 @@
 package ua.matyukhin.modul1;
 
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Task2 {
@@ -14,62 +15,52 @@ public class Task2 {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите координыты куда конь пойдет двухзначным числом: ");
         int secondInput = scan.nextInt();
-        while (secondInput % 10 > 7 || secondInput < 0) {
+        while (secondInput % 10 > 7 || secondInput > 78 || secondInput < 0) {
             System.out.println("Лимит не больше 77 и десятая доля не больше 7"); // маразматичная проверка из-за желания принимать координаты
             secondInput = scan.nextInt();                                        //одним числом,типа user friendly
         }
         int x = secondInput / 10;
         int y = secondInput % 10;
-        for (int[] ints : navigationAllow) {
-            for (int j = 0; j < ints.length; j++) {
-                if (cords[x][y] == 1) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return cords[x][y] == 1;
     }
 
     public static int[][] horseNavMesh(int[][] cords) { //определяет всевозможные ходы коня с заданой точки
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите координыта коня двухзначным числом: ");
         int firstInput = scan.nextInt();
-        while (firstInput % 10 > 7 || firstInput < 0) {
+        while (firstInput % 10 > 7 || firstInput > 78 || firstInput < 0) {
             System.out.println("Лимит не больше 77 и десятая доля не больше 7");
             firstInput = scan.nextInt();
         }
         int x = firstInput / 10;
         int y = firstInput % 10;
-        cords[x][y]=7;
-        System.out.println(Arrays.deepToString(cords).replace("],", "]\n"));
-        for (int i = 0; i < cords.length; i++) {
-            for (int j = 0; j < cords[i].length; j++) {  //Индуский код
-                if (x > 1 && y > 0) {
-                    cords[x - 2][y - 1] = 1;
-                }
-                if (x > 0 && y > 1) {
-                    cords[x - 1][y - 2] = 1;
-                }
-                if (x > 1 && y <= 6) {
-                    cords[x - 2][y + 1] = 1;
-                }
-                if (x > 0 && y < 6) {
-                    cords[x - 1][y + 2] = 1;
-                }
-                if (x <= 6 && y > 1) {
-                    cords[x + 1][y - 2] = 1;
-                }
-                if (x <= 5 && y > 0) {
-                    cords[x + 2][y - 1] = 1;
-                }
-                if (x <= 6 && y <= 5) {
-                    cords[x + 1][y + 2] = 1;
-                }
-                if (x <= 5 && y <= 6) {
-                    cords[x + 2][y + 1] = 1;
-                }
-            }
+        cords[x][y] = 7;  //показывает положение коня на "доске"
+        if (x > 1 && y > 0) {           //Индуский код
+            cords[x - 2][y - 1] = 1;
         }
+        if (x > 0 && y > 1) {
+            cords[x - 1][y - 2] = 1;
+        }
+        if (x > 1 && y <= 6) {
+            cords[x - 2][y + 1] = 1;
+        }
+        if (x > 0 && y <= 5) {
+            cords[x - 1][y + 2] = 1;
+        }
+        if (x <= 6 && y > 1) {
+            cords[x + 1][y - 2] = 1;
+        }
+        if (x <= 5 && y > 0) {
+            cords[x + 2][y - 1] = 1;
+        }
+        if (x <= 6 && y <= 5) {
+            cords[x + 1][y + 2] = 1;
+        }
+        if (x <= 5 && y <= 6) {
+            cords[x + 2][y + 1] = 1;
+        }
+        System.out.println(Arrays.deepToString(cords).replace("],", "]\n"));
         return cords;
     }
 }
+
