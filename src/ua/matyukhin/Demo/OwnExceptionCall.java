d146eac60471b2df2d67ea80a4fa8f149709984c
userwww <you@example.com>
2021-06-08 14:11:05 +0300

    no errors

--- src/ua/matyukhin/Demo/OwnExceptionCall.java
+++ src/ua/matyukhin/Demo/OwnExceptionCall.java
@@ -2,12 +2,13 @@ package ua.matyukhin.Demo;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.Reader;
 
 public class OwnExceptionCall {
     public static void testCCE(int number) {
         try {
-            Object CCE = number;
-            String text = (String) CCE;
+            Integer CCE = number;
+            int text = CCE;
         } catch (ClassCastException e) {
             e.printStackTrace();
         }
@@ -15,7 +16,7 @@ public class OwnExceptionCall {
 
     public static void testNPE(String text) {
         try {
-            BufferedReader reader = null;
+            BufferedReader reader = new BufferedReader(Reader.nullReader());
             try {
                 reader.readLine();
             } catch (IOException e) {
@@ -26,29 +27,26 @@ public class OwnExceptionCall {
             e.printStackTrace();
         }
     }
-
     public static void testNFE(String text) {
         try {
-            int i = Integer.parseInt(text);
+            int i = (Integer.parseInt(text));
         } catch (NumberFormatException e) {
             e.printStackTrace();
         }
     }
-
-    public static void testAIOOB(int number) {
-        int[] array = new int[number];
-        try {
-            int boom = array[5];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            e.printStackTrace();
-        }
-
+public static void testAIOOB (int number){
+    int[] array = new int[number];
+    try {
+        int boom = array[number-1];
+    }catch (ArrayIndexOutOfBoundsException e){
+        e.printStackTrace();
     }
 
+}
     public static void main(String[] args) {
         testCCE(777);
         testNPE("test");
-        testNFE("1test");
+        testNFE("1");
         testAIOOB(2);
     }
 }
